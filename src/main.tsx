fa69ecf71c4c010e361dcb01b1b4b0c66499feac
ShadowJak <theshadowjak@gmail.com>
2025-05-13 22:27:34 -0400

    color scheme

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-// import '@mantine/core/styles/baseline.css';
-// import '@mantine/core/styles/default-css-variables.css';
-// import '@mantine/core/styles/global.css';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { MenuBar } from './components/Menu/MenuBar.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider defaultColorScheme='auto'>
       <MenuBar />
     </MantineProvider>
   </StrictMode>,
