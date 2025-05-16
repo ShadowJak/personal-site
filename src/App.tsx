7a6256380d1396aed2a62186fd170c4175710803
ShadowJak <theshadowjak@gmail.com>
2025-05-15 21:16:45 -0400

    content box added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import classes from './App.module.scss'
+import { Content } from './components/Content/Content'
 import { MenuBar } from './components/Menu/MenuBar'
 
 function App() {
   return (
     <div className={classes.container}>
       <MenuBar />
-      <div className={classes.content}></div>
+      <Content />
     </div>
   )
 }
---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,10 @@
+import classes from './Content.module.scss';
+
+export const Content = () => {
+    return (
+        <div className={classes.content}>
+            test
+        </div>
+    )
+
+}
