f9ab506f6caa879c8c9a5cdaa95b1f23514de38d
ShadowJak <theshadowjak@gmail.com>
2025-05-23 18:03:26 -0400

    navbar junk

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import classes from './App.module.scss'
 import { Content } from './components/Content/Content'
 import { MenuBar } from './components/Menu/MenuBar'
+import { NavBar } from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className={classes.container}>
-      <MenuBar />
+      {/* <MenuBar /> */}
+      <NavBar />
       <Content />
     </div>
   )
