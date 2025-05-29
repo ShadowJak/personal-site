1ea9389f44a40d3de53df26f6b963a30d39b6c7f
ShadowJak <theshadowjak@gmail.com>
2025-05-28 21:47:45 -0400

    grainy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import classes from './App.module.scss'
 import { Content } from './components/Content/Content'
-import { MenuBar } from './components/Menu/MenuBar'
 import { Menu } from './components/Navigation/Menu'
 
 function App() {
