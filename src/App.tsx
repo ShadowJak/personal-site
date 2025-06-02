dbd756628bda15117281d758ddb2e139c10b4bdc
ShadowJak <theshadowjak@gmail.com>
2025-06-01 22:47:57 -0400

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import classes from './App.module.scss'
-import { Content } from './components/Content/Content'
-import { FullScreenNav } from './components/FullScreenNav/FullScreenNav'
-import { MenuBar } from './components/Menu/MenuBar'
 import { SlideNav } from './components/SlideNav/SlideNav'
 
 function App() {
